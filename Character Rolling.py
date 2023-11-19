a10c23b060556b1c13d44433b23a01e2a3192187
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-19 06:49:01 +0000

    Add files via upload

---
+++ Character Rolling.py
@@ -0,0 +1,40 @@
+import random
+
+def getRolls():
+    D6 = random.randint(1, 6)
+    return D6
+  
+def getLowest(die1, die2):
+    if die1 > die2:
+        lowest = die2
+    if die1 < die2:
+        lowest = die1
+    if die1 == die2:
+        lowest = die2
+    return lowest
+
+def getLowestOfFour(die1, die2, die3, die4):
+    total = 0
+    pair1 = getLowest(die1, die2)
+    pair2 = getLowest(die3, die4)
+    lowest = getLowest(pair1, pair2)
+    total = die1 + die2 + die3 + die4 - lowest
+    print("Total: " + str(total) + "\n")
+    
+def getAbilities():    
+    die1 = getRolls()
+    print("die 1: " + str(die1))
+    
+    die2 = getRolls()
+    print("die 2: " + str(die2))
+    
+    die3 = getRolls()
+    print("die 3: " + str(die3))
+    
+    die4 = getRolls()
+    print("die 4: " + str(die4))
+    
+    getLowestOfFour(die1, die2, die3, die4)
+
+for i in range(6):
+    getAbilities()
