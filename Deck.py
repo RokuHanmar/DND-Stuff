5dbf9529e2a0acb518f270292631d69edb71f79a
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-19 06:56:28 +0000

    Add files via upload

---
+++ Deck.py
@@ -0,0 +1,42 @@
+import random
+
+cards = ["Vizier\n", "Sun\n", "Moon\n", "Star\n", "Comet\n", "The Fates\n", "Throne\n", "Key\n", "Knight\n", "Gem\n", "Talons\n", "The Void\n", "Flames\n", "Skull\n", "Idiot\n", "Donjon\n", "Ruin\n", "Euryale\n", "Rogue\n", "Balance\n", "Fool\n", "Jester\n"]
+
+
+def readFile():
+    drawn = []
+    previousUses = open("Previous Uses.txt", "r")
+    for line in previousUses:
+        drawn.append(line)
+    previousUses.close()
+    return drawn
+
+
+
+def draw(numberOfCards):
+    for i in range(0, numberOfCards):
+        drawn = readFile()
+        remainingCards = len(cards) - len(drawn)
+        if remainingCards <= 0:
+            print("There are no more cards in the Deck")
+            
+        else:    
+            if remainingCards >= 0:
+                cardDrawn = random.randint(0, len(cards)-1)
+                card = cards[cardDrawn]
+                
+                while card in drawn:
+                    cardDrawn = random.randint(0, len(cards)-1)
+                    card = cards[cardDrawn]
+                previousUses = open("Previous Uses.txt", "a")
+                previousUses.write(card)
+                previousUses.close()
+                print(card)
+                remainingCards -= 1
+                i += 1
+        
+                if remainingCards <= 0:
+                    print("There are no more cards in the Deck")
+
+
+draw(int(input("How many cards are you drawing? ")))
