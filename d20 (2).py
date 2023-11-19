9e166a653fcea4b2a74c392907a80279d6d3f1f3
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-11-19 06:54:33 +0000

    Add files via upload

---
+++ d20 (2).py
@@ -0,0 +1,41 @@
+import random
+
+def rollDie():
+
+    #Get the type of die
+    die = int(input("Enter the number of sides: "))
+    while die != 4 and die != 6 and die != 8 and die != 10 and die != 12 and die != 20 and die != 100:
+        print("Invalid number. Try again")
+        die = int(input("Enter the number of sides: "))
+
+    areModifiers = input("Are there modifiers? ")
+    while areModifiers.lower() != "yes" and areModifiers.lower() != "no":
+        print("Invalid answer. Try again")
+        areModifiers = input("Are there modifiers? ")
+            
+    if areModifiers.lower() == "yes":
+        modifier = int(input("What is the modifier? "))
+    else:
+        modifier = 0
+        
+    #Get the number of dice
+    number = int(input("How many dice are you rolling? "))
+    while number < 1:
+        print("Invalid number. Try again")
+        number = int(input("How many dice are you rolling? "))
+    total = 0
+
+    if die == 20:
+        die = 7
+
+    #Roll each die
+    for i in range(number):
+        roll = random.randint(1, die)
+        modifiedRoll = roll + modifier
+        print("Die", str(i + 1) + " (without modifiers):", str(roll) + "\n")
+        print("Die", str(i + 1) + " (with modifiers):", str(modifiedRoll) + "\n")
+        total += modifiedRoll
+    print("Total: " + str(total))
+
+
+rollDie()
