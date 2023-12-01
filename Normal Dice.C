e1b3a20a0366df47326aa68fcea99f1321e70a72
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-01 07:37:07 +0000

    Update Normal Dice.C
    Added modifiers to rolls

--- Normal Dice.C
+++ Normal Dice.C
@@ -6,15 +6,20 @@ int main() {
     srand ((unsigned) time (&t6));
 
     // Get number of sides
-    printf("%c\n", "Enter the number of sides: ");
+    printf("%d\n", "Enter the number of sides: ");
     int sides;
-    scanf("%c", &sides);
+    scanf("%d", &sides);
 
     // Verify valid die
     while (sides != 2 && sides != 3 && sides != 4 && sides != 6 && sides != 8 && sides != 10 && sides != 12 && sides != 20 && sides != 100) {
-    printf("%c\n", ". Invalid. Enter the number of sides: ");
+    printf("%d\n", ". Invalid. Enter the number of sides: ");
     scanf("%d", &sides);
     }
+
+    // Get modifiers
+    printf("%d\n", "Enter modifier. If there are none, enter 0: ");
+    int modifier;
+    scanf("%d", modifier);
 
 
 
